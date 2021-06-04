0ed24c07a8a71b723cdd2dfd0a81f9a53d78706b
ashutosh <yadav.ashu2496@gmail.com>
2021-06-04 18:10:36 +0530

    webapp-test8 down req files

--- pipeline.groovy
+++ pipeline.groovy
@@ -13,12 +13,12 @@ def buildimage() {
     //sh 'docker push yadavashu/demo:$BUILD_ID'
     
     sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( remoteDirectory: '//home//ubuntu//docker//', sourceFiles: 'setup.yml')])])
-    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cd /home/ubuntu/docker/ ; wget https://raw.githubusercontent.com/hiashutosh/jenkins/master/Dockerfile ; wget https://raw.githubusercontent.com/hiashutosh/jenkins/master/setup.yml; ansible-playbook -i 192.168.132.146, setup.yml''')])])
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cd /home/ubuntu/docker/ ; wget https://raw.githubusercontent.com/hiashutosh/jenkins/master/Dockerfile ; wget https://raw.githubusercontent.com/hiashutosh/jenkins/master/setup.yml; ansible-playbook -i 192.168.132.146, setup.yml --extra-vars "ansible_password=ashu1234"''')])])
 }
 
 def deploy() {
     sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( remoteDirectory: '//home//ubuntu//docker//', sourceFiles: 'test-playbook.yml')])])
-    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cd /home/ubuntu/docker/ ; wget https://raw.githubusercontent.com/hiashutosh/jenkins/master/test-playbook.yml ; ansible-playbook -i 192.168.132.146, test-playbook.yml''')])])
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cd /home/ubuntu/docker/ ; wget https://raw.githubusercontent.com/hiashutosh/jenkins/master/test-playbook.yml ; ansible-playbook -i 192.168.132.146, test-playbook.yml --extra-vars "ansible_password=ashu1234"''')])])
 
 }
 
