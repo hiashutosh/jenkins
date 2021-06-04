986bf8348d68fee9b143a357f166c7c8d80930b3
ashutosh <yadav.ashu2496@gmail.com>
2021-06-04 17:38:44 +0530

    webapp-test6

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,15 +3,19 @@ def buildjar() {
 }
 
 def buildimage() {
-    sh 'cd /home/ubuntu/docker'
+    
     sh 'ansible-playbook -i 192.168.132.146, setup.yml'
     //sh 'docker build -t yadavashu/demo:$BUILD_ID .'
     //sh 'docker push yadavashu/demo:$BUILD_ID'
-        
+
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( remoteDirectory: '//home//ubuntu//docker//', sourceFiles: 'setup.yml')])])
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cd /home/ubuntu/docker ; ansible-playbook -i 192.168.132.146, setup.yml''')])])
 }
 
 def deploy() {
-    sh 'ansible-playbook -i 192.168.132.146, test-playbook.yml'
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( remoteDirectory: '//home//ubuntu//docker//', sourceFiles: 'test-playbook.yml')])])
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cd /home/ubuntu/docker ; ansible-playbook -i 192.168.132.146, test-playbook.yml''')])])
+
 }
 
 return this
