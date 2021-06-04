a2b5c9fb8c080ce72d2bfa11963005042d1e7067
ashutosh <yadav.ashu2496@gmail.com>
2021-06-04 16:34:29 +0530

    webapp-test1

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ def buildjar() {
 }
 
 def buildimage() {
-    sh 'ansible-playbook -i 192.168.132.146, --ssh-extra-args='-o StrictHostKeyChecking=no' setup.yml'
+    sh "ansible-playbook -i 192.168.132.146, --ssh-extra-args='-o StrictHostKeyChecking=no' setup.yml'"
     //sh 'docker build -t yadavashu/demo:$BUILD_ID .'
     //sh 'docker push yadavashu/demo:$BUILD_ID'
         
