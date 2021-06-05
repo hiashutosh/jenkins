8df5f360735c12c7c2df4602e9ee0c5d05532d74
ashutosh <yadav.ashu2496@gmail.com>
2021-06-05 15:14:06 +0530

    using sshPublisher to wrire vars

--- pipeline.groovy
+++ pipeline.groovy
@@ -11,12 +11,12 @@ def buildimage() {
     //sh 'ansible-playbook -i 192.168.132.146, setup.yml'
     //sh 'docker build -t yadavashu/demo:$BUILD_ID .'
     //sh 'docker push yadavashu/demo:$BUILD_ID'
-    sh '''cat >> /home/ubuntu/docker/varsfile.yml << EOF
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cat >> /home/ubuntu/docker/varsfile.yml << EOF
     build_id: ${currentBuild.number}
     job_name: $JOB_NAME
     build_id_old:${currentBuild.previousBuild.getNumber()}
     job_name_old: $JOB_NAME
-    EOF'''
+    EOF''')]verbose: true)])
     
     sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( remoteDirectory: '//home//ubuntu//docker//', sourceFiles: 'setup.yml')], verbose:true)])
     sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cd /home/ubuntu/docker/ ; wget https://raw.githubusercontent.com/hiashutosh/jenkins/master/Dockerfile ; wget https://raw.githubusercontent.com/hiashutosh/jenkins/master/setup.yml; ansible-playbook -i 192.168.132.146, setup.yml --extra-vars "ansible_password=ashu1234"''')], verbose:true)])
