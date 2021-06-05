684d72919e579c82b540f635f83567f49c936a56
ashutosh <yadav.ashu2496@gmail.com>
2021-06-06 00:11:08 +0530

    --restart=always

--- pipeline.groovy
+++ pipeline.groovy
@@ -20,7 +20,7 @@ build_id_old: $(( $BUILD_NUMBER-1 ))
 job_name_old: test
 version:
 EOF
-for i in {0..$BUILD_NUMBER}
+for i in {0..$(( $BUILD_NUMBER-1 ))}
 do
 arr[$i]=$i
 cat >> /home/ubuntu/docker/varsfile.yml << EOF
