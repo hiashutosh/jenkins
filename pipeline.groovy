aee9635441f0277ca5faff3202278e41fddf4f81
ashutosh <yadav.ashu2496@gmail.com>
2021-06-05 15:52:56 +0530

    using sshPublisher to wrire vars

--- pipeline.groovy
+++ pipeline.groovy
@@ -12,9 +12,9 @@ def buildimage() {
     //sh 'docker build -t yadavashu/demo:$BUILD_ID .'
     //sh 'docker push yadavashu/demo:$BUILD_ID'
     sshPublisher(publishers: [sshPublisherDesc(configName: 'docker', transfers: [sshTransfer( execCommand: '''cat >> /home/ubuntu/docker/varsfile.yml << EOF
-    build_id: ${currentBuild}
+    build_id: currentBuild.getNumber()
     job_name: $JOB_NAME
-    build_id_old:${currentBuild.previousBuild}
+    build_id_old: currentBuild.getPreviousBuild()
     job_name_old: $JOB_NAME
 EOF''')], verbose: true)])
     
